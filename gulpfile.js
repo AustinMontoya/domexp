080662813ac1f777745c16e3c4d5d0ac3a825e5c
Austin Montoya <austin.montoya.ctr@adlnet.gov>
2014-04-01 21:39:37 -0400

    getting the build tools in somewhat working order

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,26 @@ var coffee = require('gulp-coffee');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function() {
-	gulp.src('test/**/*.coffee')
-		.pipe(coffee({bare: true}).on('error', gutil.log))
-		.pipe(gulp.dest('test'))
-		.pipe(mocha({reporter: 'list'}));
+	gulp.src('bin/test/**/*.js')
+		.pipe(mocha({reporter: 'list', growl: true})).on('error', function(){});
 });
+
+gulp.task('compile', function() {
+  var coffeeCompile = function(src, dest) {
+    gulp.src(src)
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest(dest));
+  };
+
+  coffeeCompile('src/**/*.coffee', 'bin');
+  coffeeCompile('test/**/*.coffee', 'bin/test');
+
+  gulp.src(['node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('bin/lib'));
+})
+
+gulp.task('watch', function() {
+	gulp.watch('./**/*.coffee', ['compile', 'test']);
+});
+
+gulp.task('default', ['compile', 'test', 'watch'])
